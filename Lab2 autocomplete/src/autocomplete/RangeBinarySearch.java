f1f2cc959ce84bd4edc0667c8f197e0156d6adb7
Tobias <Tobias@Tobias-Dator>
2020-04-19 20:31:08 +0200

    first commit

---
+++ Lab2 autocomplete/src/autocomplete/RangeBinarySearch.java
@@ -0,0 +1,13 @@
+package autocomplete;
+
+public class RangeBinarySearch {
+	 // Returns the index of the first key in a[] that equals the search key, or -1 if no such key exists.
+    // Complexity: O(log N), N = a.length.
+    // @pre a is sorted.
+    public static int firstIndexOf(Key[] a, Key key, Comparator comparator);
+
+    // Returns the index of the last key in a[] that equals the search key, or -1 if no such key exists.
+    // Complexity: O(log N), N = a.length
+    // @pre a is sorted.
+    public static int lastIndexOf(Key[] a, Key key, Comparator comparator);
+}
---
+++ Lab2 autocomplete/src/autocomplete/Term.java
@@ -0,0 +1,32 @@
+package autocomplete;
+
+public class Term {
+	
+	private long weight;
+	private String query;
+
+	// Initializes a term with the given query string and weight.
+	public Term(String query, long weight) {
+		this.query = query;
+		this.weight = weight;
+	}
+
+	// Compares the two terms in lexicographic order by query.
+	public static Comparator<Term> byLexicographicOrder(){
+		
+	}
+
+	// Compares the two terms in descending order by weight.
+	public static Comparator<Term> byReverseWeightOrder();
+
+	// Compares the two terms in lexicographic order,
+	// but using only the first k characters of each query.
+	public static Comparator<Term> byPrefixOrder(int k);
+
+	// Returns a string representation of this term in the following format:
+	// the weight, followed by whitespace, followed by the query.
+	public String toString() {
+		return String.format("%12d    %s", this.weight, this.query);
+	}
+
+}
