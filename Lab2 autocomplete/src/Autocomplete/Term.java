64aa3ca1f65ad278ca25480a248b961e8ee12a5a
Tobias <Tobias@Tobias-Dator>
2020-04-20 09:22:44 +0200

    comparator

--- Lab2 autocomplete/src/Autocomplete/Term.java
+++ Lab2 autocomplete/src/Autocomplete/Term.java
@@ -1,9 +1,11 @@
 package Autocomplete;
 
+import java.util.Comparator;
+
 public class Term {
 	
-	private long weight;
-	private String query;
+	public long weight;
+	public String query;
 
 	// Initializes a term with the given query string and weight.
 	public Term(String query, long weight) {
@@ -13,11 +15,39 @@ public class Term {
 
 	// Compares the two terms in lexicographic order by query.
 	public static Comparator<Term> byLexicographicOrder(){
-		
+		// Detta är fel
+		Comparator<Term> com = new Comparator<Term>() {
+			public int compare(Term term1, Term term2) {
+				if(term1.weight > term2.weight) {
+					return 1;
+				}
+				else {
+					return -1;
+				}
+			}
+			
+		}; 
+			
+		return com;
 	}
 
 	// Compares the two terms in descending order by weight.
-	public static Comparator<Term> byReverseWeightOrder();
+	public static Comparator<Term> byReverseWeightOrder(){
+		//decending order?
+		Comparator<Term> com = new Comparator<Term>() {
+			public int compare(Term term1, Term term2) {
+				if(term1.weight > term2.weight) {
+					return 1;
+				}
+				else {
+					return -1;
+				}
+			}
+			
+		}; 
+			
+		return com;
+	}
 
 	// Compares the two terms in lexicographic order,
 	// but using only the first k characters of each query.
