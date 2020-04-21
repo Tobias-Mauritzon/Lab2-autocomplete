3cb0335db87a4cc966297c52e02333bebb1ef99b
Tobias <Tobias@Tobias-Dator>
2020-04-21 16:13:54 +0200

    RangeBinarySearch klar tror jag

--- Lab2 autocomplete/src/Autocomplete/RangeBinarySearch.java
+++ Lab2 autocomplete/src/Autocomplete/RangeBinarySearch.java
@@ -1,13 +1,21 @@
 package Autocomplete;
 
+import java.awt.RenderingHints.Key;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class RangeBinarySearch {
 	 // Returns the index of the first key in a[] that equals the search key, or -1 if no such key exists.
     // Complexity: O(log N), N = a.length.
     // @pre a is sorted.
-    public static int firstIndexOf(Key[] a, Key key, Comparator comparator);
+    public static int firstIndexOf(Key[] a, Key key, Comparator comparator) {
+    	return Arrays.binarySearch(a, key, comparator);
+    }
 
     // Returns the index of the last key in a[] that equals the search key, or -1 if no such key exists.
     // Complexity: O(log N), N = a.length
     // @pre a is sorted.
-    public static int lastIndexOf(Key[] a, Key key, Comparator comparator);
+    public static int lastIndexOf(Key[] a, Key key, Comparator comparator) {
+    	return Arrays.binarySearch(a, a.length-1, 0, key, comparator);
+    }
 }
