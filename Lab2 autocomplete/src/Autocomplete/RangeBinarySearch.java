7375da5421f6f6a3080036643b9858b69f706fd9
joantfolk <antfolk@student.chalmers.se>
2020-04-21 16:40:13 +0200

    generisk comparator

--- Lab2 autocomplete/src/Autocomplete/RangeBinarySearch.java
+++ Lab2 autocomplete/src/Autocomplete/RangeBinarySearch.java
@@ -7,14 +7,14 @@ public class RangeBinarySearch {
 	 // Returns the index of the first key in a[] that equals the search key, or -1 if no such key exists.
     // Complexity: O(log N), N = a.length.
     // @pre a is sorted.
-    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator comparator) {
+    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
     	return Arrays.binarySearch(a, key, comparator);
     }
 
     // Returns the index of the last key in a[] that equals the search key, or -1 if no such key exists.
     // Complexity: O(log N), N = a.length
     // @pre a is sorted.
-    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator comparator) {
+    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
     	return Arrays.binarySearch(a, a.length-1, 0, key, comparator);
     }
 }
