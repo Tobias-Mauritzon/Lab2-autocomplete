a0d13ccc3134e5f0e6ef509aeb747f3a6cc5eacb
Tobias <Tobias@Tobias-Dator>
2020-04-22 10:18:09 +0200

    Ändringar

--- Lab2 autocomplete/src/Autocomplete/RangeBinarySearch.java
+++ Lab2 autocomplete/src/Autocomplete/RangeBinarySearch.java
@@ -8,7 +8,24 @@ public class RangeBinarySearch {
     // Complexity: O(log N), N = a.length.
     // @pre a is sorted.
     public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
-    	return Arrays.binarySearch(a, key, comparator);
+    	
+    		int low = 0;
+    		int high = a.length - 1;
+
+    		while (low <= high) {
+    			int mid = (low + high) >>> 1;
+    			Key midVal = a[mid];
+    			int cmp = comparator.compare(midVal, key);
+    			if (cmp < 0)
+    				low = mid + 1;
+    			else if (cmp > 0)
+    				high = mid - 1;
+    			else
+    				return mid; // key found
+    		}
+    		return -1;  // key not found.
+    	
+    	
     }
 
     // Returns the index of the last key in a[] that equals the search key, or -1 if no such key exists.
