d6f69d2c5c45f6f595b13ef3a2965da54735ecb7
Tobias <Tobias@Tobias-Dator>
2020-04-22 14:00:38 +0200

    Hej

--- Lab2 autocomplete/src/Autocomplete/RangeBinarySearch.java
+++ Lab2 autocomplete/src/Autocomplete/RangeBinarySearch.java
@@ -11,14 +11,23 @@ public class RangeBinarySearch {
 	public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
 		if(a == null || key == null || comparator == null) {
 			throw new IllegalArgumentException();
+		}		
+		
+		int low = 0;
+		int high = a.length - 1;
+		
+		while (low <= high) {
+			int mid = (low + high) /2;
+			Key midVal = a[mid];
+			int cmp = comparator.compare(key, midVal);
+			if (cmp < 0)
+				low = mid + 1;
+			else if (cmp > 0)
+				high = mid - 1;
+			else
+				return mid; // key found
 		}
-			
-		int ret = Arrays.binarySearch(a, key, comparator);
-		if (ret < 0) {
-			return -1;
-		} else {
-			return ret;
-		}
+		return -1;  // key not found.
 
 	}
 
@@ -30,17 +39,14 @@ public class RangeBinarySearch {
 		if(a == null || key == null || comparator == null) {
 			throw new IllegalArgumentException();
 		}
-		//return Arrays.binarySearch(a, a.length - 1, 0, key, comparator);
-		
-		//private static <T> int binarySearch0(T[] a, int fromIndex, int toIndex, T key, Comparator<? super T> c) {
-			
+	
 			int low = 0;
 			int high = a.length - 1;
 			
 			while (low <= high) {
-				int mid = (low + high) >>> 1;
+				int mid = (low + high) /2;
 				Key midVal = a[mid];
-				int cmp = comparator.compare(midVal, key);
+				int cmp = comparator.compare(key, midVal);
 				if (cmp < 0)
 					low = mid + 1;
 				else if (cmp > 0)
