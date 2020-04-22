8b5bdff071e3adf3caed92e7322cb9b00a283a80
Tobias <Tobias@Tobias-Dator>
2020-04-22 13:53:35 +0200

    123

--- Lab2 autocomplete/src/Autocomplete/RangeBinarySearch.java
+++ Lab2 autocomplete/src/Autocomplete/RangeBinarySearch.java
@@ -30,6 +30,26 @@ public class RangeBinarySearch {
 		if(a == null || key == null || comparator == null) {
 			throw new IllegalArgumentException();
 		}
-		return Arrays.binarySearch(a, a.length - 1, 0, key, comparator);
+		//return Arrays.binarySearch(a, a.length - 1, 0, key, comparator);
+		
+		//private static <T> int binarySearch0(T[] a, int fromIndex, int toIndex, T key, Comparator<? super T> c) {
+			
+			int low = 0;
+			int high = a.length - 1;
+			
+			while (low <= high) {
+				int mid = (low + high) >>> 1;
+				Key midVal = a[mid];
+				int cmp = comparator.compare(midVal, key);
+				if (cmp < 0)
+					low = mid + 1;
+				else if (cmp > 0)
+					high = mid - 1;
+				else
+					return mid; // key found
+			}
+			return -(low + 1);  // key not found.
+		
+		
 	}
 }
