7d34036ade8d6eb26bba46039af30ab83b157c49
joantfolk <antfolk@student.chalmers.se>
2020-04-22 14:00:01 +0200

    term

--- Lab2 autocomplete/src/Autocomplete/Term.java
+++ Lab2 autocomplete/src/Autocomplete/Term.java
@@ -67,8 +67,16 @@ public class Term {
 		Comparator<Term> com = new Comparator<Term>() {
 			public int compare(Term term1, Term term2) {
 				// Kan nog skippa att declarera Stringsen för bättre prestanda
-				String term1K = term1.query.substring(0, k);
-				String term2K = term2.query.substring(0, k);
+				int index = k;
+				if(index > term1.query.length() || index > term2.query.length()) {
+					if(term1.query.length() >= term2.query.length()) {
+						index = term2.query.length();
+					}else {
+						index = term1.query.length();
+					}
+				}
+				String term1K = term1.query.substring(0, index - 1);
+				String term2K = term2.query.substring(0, index - 1);
 				if (term1K.compareToIgnoreCase(term2K) < 0) {
 					return 1;
 				} else if (term1K.compareToIgnoreCase(term2K) > 0) {
@@ -88,5 +96,4 @@ public class Term {
 	public String toString() {
 		return String.format("%12d    %s", this.weight, this.query);
 	}
-
 }
