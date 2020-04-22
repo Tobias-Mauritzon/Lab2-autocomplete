368fb41abe9ec6725952ba329b3b4f80731ac3c6
joantfolk <antfolk@student.chalmers.se>
2020-04-22 13:33:49 +0200

    felhantering

--- Lab2 autocomplete/src/Autocomplete/RangeBinarySearch.java
+++ Lab2 autocomplete/src/Autocomplete/RangeBinarySearch.java
@@ -4,24 +4,32 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class RangeBinarySearch {
-	 // Returns the index of the first key in a[] that equals the search key, or -1 if no such key exists.
-    // Complexity: O(log N), N = a.length.
-    // @pre a is sorted.
-    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
-    	int ret = Arrays.binarySearch(a, key, comparator);
-    	if(ret < 0) {
-    		return -1;
-    	}
-    	else {
-    		return ret;
-    	}
-    		
-    }
+	// Returns the index of the first key in a[] that equals the search key, or -1
+	// if no such key exists.
+	// Complexity: O(log N), N = a.length.
+	// @pre a is sorted.
+	public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
+		if(a == null || key == null || comparator == null) {
+			throw new IllegalArgumentException();
+		}
+			
+		int ret = Arrays.binarySearch(a, key, comparator);
+		if (ret < 0) {
+			return -1;
+		} else {
+			return ret;
+		}
 
-    // Returns the index of the last key in a[] that equals the search key, or -1 if no such key exists.
-    // Complexity: O(log N), N = a.length
-    // @pre a is sorted.
-    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
-    	return Arrays.binarySearch(a, a.length-1, 0, key, comparator);
-    }
+	}
+
+	// Returns the index of the last key in a[] that equals the search key, or -1 if
+	// no such key exists.
+	// Complexity: O(log N), N = a.length
+	// @pre a is sorted.
+	public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
+		if(a == null || key == null || comparator == null) {
+			throw new IllegalArgumentException();
+		}
+		return Arrays.binarySearch(a, a.length - 1, 0, key, comparator);
+	}
 }
--- Lab2 autocomplete/src/Autocomplete/Term.java
+++ Lab2 autocomplete/src/Autocomplete/Term.java
@@ -4,84 +4,82 @@ import java.util.Comparator;
 import java.util.Objects;
 
 public class Term {
-	
+
 	public long weight;
 	public String query;
 
 	// Initializes a term with the given query string and weight.
 	public Term(String query, long weight) {
 		if (weight < 0) {
-            throw new IllegalArgumentException();
+			throw new IllegalArgumentException();
 		}
-		this.query = Objects.requireNonNull(query);
-		this.weight = Objects.requireNonNull(weight);
+		try {
+			this.query = Objects.requireNonNull(query);
+			this.weight = Objects.requireNonNull(weight);
+		} catch (NullPointerException e) {
+			throw e;
+		}
 	}
 
 	// Compares the two terms in lexicographic order by query.
-	public static Comparator<Term> byLexicographicOrder(){
+	public static Comparator<Term> byLexicographicOrder() {
 		Comparator<Term> com = new Comparator<Term>() {
 			public int compare(Term term1, Term term2) {
-				if(term1.query.compareToIgnoreCase(term2.query) < 0) {
+				if (term1.query.compareToIgnoreCase(term2.query) < 0) {
 					return 1;
-				}
-				else if(term1.query.compareToIgnoreCase(term2.query) > 0){
+				} else if (term1.query.compareToIgnoreCase(term2.query) > 0) {
 					return -1;
-				}
-				else {
+				} else {
 					return 0;
 				}
 			}
-			
-		}; 
-			
+
+		};
+
 		return com;
 	}
 
 	// Compares the two terms in descending order by weight.
-	public static Comparator<Term> byReverseWeightOrder(){
-		//decending order?
+	public static Comparator<Term> byReverseWeightOrder() {
+		// decending order?
 		Comparator<Term> com = new Comparator<Term>() {
 			public int compare(Term term1, Term term2) {
-				if(term1.weight > term2.weight) {
+				if (term1.weight > term2.weight) {
 					return 1;
-				}
-				else if (term1.weight < term2.weight){
+				} else if (term1.weight < term2.weight) {
 					return -1;
-				}
-				else {
+				} else {
 					return 0;
 				}
 			}
-			
-		}; 
-			
+
+		};
+
 		return com;
 	}
 
 	// Compares the two terms in lexicographic order,
 	// but using only the first k characters of each query.
-	public static Comparator<Term> byPrefixOrder(int k){
+	public static Comparator<Term> byPrefixOrder(int k) {
 		if (k < 0) {
-            throw new IllegalArgumentException();
+			throw new IllegalArgumentException();
 		}
 		Comparator<Term> com = new Comparator<Term>() {
 			public int compare(Term term1, Term term2) {
 				// Kan nog skippa att declarera Stringsen för bättre prestanda
 				String term1K = term1.query.substring(0, k);
 				String term2K = term2.query.substring(0, k);
-				if(term1K.compareToIgnoreCase(term2K) < 0) {
+				if (term1K.compareToIgnoreCase(term2K) < 0) {
 					return 1;
-				}
-				else if(term1K.compareToIgnoreCase(term2K) > 0){
+				} else if (term1K.compareToIgnoreCase(term2K) > 0) {
 					return -1;
-				}
-				else {
+				} else {
 					return 0;
 				}
 			}
-			
-		}; 
-			
+
+		};
+
 		return com;
 	}
 
