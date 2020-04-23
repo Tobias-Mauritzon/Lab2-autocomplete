23e2c0450394140f42a8808c4f1d72d6578a0682
joantfolk <antfolk@student.chalmers.se>
2020-04-23 13:46:51 +0200

    push

--- Lab2 autocomplete/src/Autocomplete/Autocomplete.java
+++ Lab2 autocomplete/src/Autocomplete/Autocomplete.java
@@ -3,39 +3,70 @@ package Autocomplete;
 import java.util.Arrays;
 
 public class Autocomplete {
-	Term[] terms;
-    // Initializes the data structure from the given array of terms.
-    // Complexity: O(N log N), where N is the number of terms
-    public Autocomplete(Term[] terms) {
-    	this.terms = terms;
-    }
+	private Term[] terms;
 
-    // Returns all terms that start with the given prefix, in descending order of weight.
-    // Complexity: O(log N + M log M), where M is the number of matching terms
-    public Term[] allMatches(String prefix){
-    	Term term = new Term(prefix, 0);
-    	int firstIndex = RangeBinarySearch.firstIndexOf(terms, term, Term.byPrefixOrder(prefix.length()));
-    	int lastIndex = RangeBinarySearch.lastIndexOf(terms, term, Term.byPrefixOrder(prefix.length()));
-    	
-    	//Checka om last first samma
-    	//Om man inte får tillbaka något
-    	int length = lastIndex - firstIndex;
-    	
-    	Term[] retTerms = new Term[length];
-    	
-    	int i = 0;
-    	while(firstIndex <= lastIndex) {
-    		retTerms[i] = terms[firstIndex];
-    		i++;
-    		firstIndex++;
-    	}
-    	Arrays.sort(retTerms, Term.byReverseWeightOrder());
-    	return retTerms;
-    }
+	// Initializes the data structure from the given array of terms.
+	// Complexity: O(N log N), where N is the number of terms
+	public Autocomplete(Term[] terms) {
+		
+		
+		
+		this.terms = terms;
+	}
 
-    // Returns the number of terms that start with the given prefix.
-    // Complexity: O(log N)
-    public int numberOfMatches(String prefix) {
-    	return 0;
-    }
+	// Returns all terms that start with the given prefix, in descending order of
+	// weight.
+	// Complexity: O(log N + M log M), where M is the number of matching terms
+	public Term[] allMatches(String prefix) {
+		
+		if(prefix == null) {
+			throw new NullPointerException();
+		}
+		Term term = new Term(prefix, 0);
+		int firstIndex = RangeBinarySearch.firstIndexOf(terms, term, Term.byPrefixOrder(prefix.length()));
+		
+		if(firstIndex >= 0) {
+			int lastIndex = RangeBinarySearch.lastIndexOf(terms, term, Term.byPrefixOrder(prefix.length()));
+
+			if(firstIndex == lastIndex) {
+				Term[] retTerms = new Term[1];
+				
+				retTerms[0] = terms[firstIndex];
+				return retTerms;
+			}else {
+				int length = lastIndex - firstIndex;
+
+				Term[] retTerms = new Term[length];
+
+				int i = 0;
+				while (firstIndex <= lastIndex) {
+					retTerms[i] = terms[firstIndex];
+					i++;
+					firstIndex++;
+				}
+				Arrays.sort(retTerms, Term.byReverseWeightOrder());
+				return retTerms;
+			}
+		}else {
+			return new Term[0];
+		}
+		
+	}
+
+	// Returns the number of terms that start with the given prefix.
+	// Complexity: O(log N)
+	public int numberOfMatches(String prefix) {
+		if(prefix == null) {
+			throw new NullPointerException();
+		}
+		
+		Term term = new Term(prefix, 0);
+		int firstIndex = RangeBinarySearch.firstIndexOf(terms, term, Term.byPrefixOrder(prefix.length()));
+		int lastIndex = RangeBinarySearch.lastIndexOf(terms, term, Term.byPrefixOrder(prefix.length()));
+		if (firstIndex < 0 || lastIndex < 0) {
+			return 0;
+		} else {
+			return lastIndex - firstIndex;
+		}
+	}
 }
